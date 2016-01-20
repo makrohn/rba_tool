89dd610725bacd36608cc868ddd9849f193181a3
Matthew Krohn <mkrohn@dyn.com>
2016-01-19 20:46:36 -0500

    Basic view and template

--- views.py
+++ views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader
+from .models import Role, Service, Access
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the RBA index.")
+    role_list = Role.objects.order_by('role_name')
+    template = loader.get_template('rba/index.html')
+    context = {
+        'role_list': role_list,
+    }
+    return HttpResponse(template.render(context, request))
+
