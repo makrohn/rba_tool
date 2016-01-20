826c92434864da0a9a0ae2bce1b7352fc354d97a
Matthew Krohn <mkrohn@dyn.com>
2016-01-19 22:21:13 -0500

    Starting to modify views and templates

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
-from .models import Role, Service, Access
+from .models import Role, Service, Access, Team
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 
@@ -13,3 +13,16 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+def accessDisplay(request, total_access):
+    template = loader.get_template('rba/access.html')
+    context = {
+        'access_list': total_access,
+    }
+    return HttpResponse(template.render(context, request))
+
+def calc_access(request, role_id):
+    role = Role.objects.get(role_name=role_id)
+    total_access={}
+    for item in Access.objects.filter(associated_role=role):
+        total_access[item.associated_service] = item.access_level
+    return HttpResonseRedirect(reverse('accessDisplay',args=(total_access)))
