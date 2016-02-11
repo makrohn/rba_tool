d3392766582ec50fffa37639474dedb0c5b3933b
Matthew Krohn <mkrohn@dyn.com>
2016-02-11 13:56:47 -0500

    Add new roles from left-hand column of CSV, check memberships

--- importer.py
+++ importer.py
@@ -16,3 +16,31 @@ def import_services(service_list):
         if service not in current_services:
             new_service = Service(service_name=service)
             new_service.save()
+
+def read_new_roles(file):
+    new_roles = []
+    with open(file) as access_file:
+        for line in access_file.readlines()[1:]:
+            new_roles.append(line.strip().split(",")[0])
+    return new_roles
+
+def read_current_roles():
+    current_roles = []
+    for role in Role.objects.all():
+        current_roles.append(role.role_name)
+    return current_roles
+
+def import_roles(role_list):
+    current_roles = read_current_roles()
+    for role in role_list:
+        if role not in current_roles:
+            new_role = Role(role_name=role)
+            new_role.save()
+
+def read_new_memberships(file):
+    new_memberships = {}
+    with open(file) as access_file:
+        for line in access_file.readlines()[1:]:
+            new_memberships[line.strip().split(",")[0]]\
+                = line.strip().split(",")[1]
+    return new_memberships
