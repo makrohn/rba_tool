79b1dd0e2c03870e435445ca4c552e08fab520c3
Matthew Krohn <mkrohn@dyn.com>
2016-02-16 13:38:33 -0500

    Improve according to pep8 standards

--- views.py
+++ views.py
@@ -6,6 +6,7 @@ from .models import Role, Service, Access, Team
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 
+
 def index(request):
     """Generate list of clickable links to find access"""
     role_list = Role.objects.order_by('role_name')
@@ -15,15 +16,18 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def get_access(role):
     """Compile all accesses for a given role"""
-    role_access={}
+    role_access = {}
     for item in Access.objects.filter(associated_role=role):
-        role_access[item.associated_service.service_name] = str(item.access_level).split(",")
+        role_access[item.associated_service.service_name] = \
+            str(item.access_level).split(",")
     return role_access
 
+
 def access_results(request, role_id):
-    """When someone clicks a link, find all systems that role needs access to"""
+    """Find all systems that role needs access to"""
     role = Role.objects.get(pk=role_id)
     total_access = get_access(role)
     roles_checked = [role]
@@ -32,7 +36,7 @@ def access_results(request, role_id):
             """Prevent infinite loops if role is eventually member of itself"""
             response = "Role loop detected at %s!"
             return HttpResponse(response % role)
-        privileges=get_access(role.membership)
+        privileges = get_access(role.membership)
         for privilege in privileges:
             if privilege in total_access:
                 for priv in privileges[privilege]:
